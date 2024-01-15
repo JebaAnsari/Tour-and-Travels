263201ec583e4dec762495ec3b8b05cddd5fcf5b
Jeba Ansari <114054093+JebaAnsari@users.noreply.github.com>
2024-01-15 15:04:51 +0530

    Add files via upload

---
+++ images/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<section class="about">
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>Why choose us?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis, unde dolorem optio tenetur laborum autem officia alias rem. Tempora, soluta quam quo eaque neque aspernatur corporis itaque. Iusto animi officia eos. Corrupti sapiente non voluptates ipsam similique pariatur quisquam aspernatur!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae minus eos amet quo dolorem ducimus, quaerat eum praesentium mollitia minima!</p>
+        <div class="icons-container">
+            <div class="icons">
+                <i class="fas fa-map"></i> 
+                <span>top destinations</span>
+            </div>
+
+            <div class="icons">
+                <i class="fas fa-han-holding-usd"></i> 
+                <span>affordable prices</span>
+            </div>
+
+            <div class="icons">
+                <i class="fas fa-headset"></i> 
+                <span>24/7 guide seervice</span>
+            </div>
+
+
+        </div>
+    </div>
+
+</section>
+</body>
+</html>
